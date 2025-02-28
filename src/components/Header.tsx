9dccff7fbf13599908df541dd0646a11de5acfac
Isa Westling <isawestling.blogg@hotmail.com>
2025-02-28 13:57:58 +0100

    Lagt till användarhantering i Header-komponenten för att visa inloggningsstatus och logout-funktionalitet

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,25 @@
 import { NavLink } from "react-router-dom" // Importera NavLink
+import { useAuth } from "../context/AuthContext"
 
 const Header = () => {
+
+  const { user, logout } = useAuth(); // Importera user och logout från AuthContext
+
   return (
     <header>
-        <nav>
-            <ul>
-                <li><NavLink to="/">Hem</NavLink></li>
-                <li><NavLink to="/logga-in">Logga in</NavLink></li>
-            </ul>
-        </nav>
+      <nav>
+        <ul>
+          <li><NavLink to="/">Hem</NavLink></li>
+          <li>
+            {/* Om användaren är inloggad, visa länk till /mina-sidor */}
+            {user && <NavLink to="/mina-sidor">Mina sidor</NavLink>}
+          </li>
+          <li>
+            {/* Om användaren inte är inloggad, visa länk till /mina-sidor, annars visa knapp för att logga ut */}
+            {!user ? <NavLink to="/mina-sidor">Logga in</NavLink> : <button onClick={logout}>Logga ut</button>}
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
