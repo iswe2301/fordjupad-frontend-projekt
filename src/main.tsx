161add86aa465501e11a65ec5e2b421f122f24f4
Isa Westling <isawestling.blogg@hotmail.com>
2025-02-28 13:57:32 +0100

    Lagt till AuthProvider för att hantera autentisering i applikationen

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom' // Importera RouterProvider
 import './index.css'
 import router from './routing.tsx' // Importera routern
+import { AuthProvider } from './context/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} /> {/* Använd RouterProvider */}
+    <AuthProvider> {/* Använd AuthProvider */}
+      <RouterProvider router={router} /> {/* Använd RouterProvider */}
+    </AuthProvider>
   </StrictMode>,
 )
