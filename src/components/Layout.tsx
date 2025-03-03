28bb6432895c77e51989e63bf1ff08e249b5381f
Isa Westling <isawestling.blogg@hotmail.com>
2025-03-03 19:57:11 +0100

    Lagt till container-klass för main

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,11 +2,12 @@ import Header from "./Header" // Importera Header
 import Footer from "./Footer" // Importera Footer
 import { Outlet } from "react-router-dom"
 
+// Layout-komponent
 const Layout = () => {
     return (
         <>
             <Header /> {/* Rendera Header */}
-            <main>
+            <main className="container my-4"> {/* Container för innehållet */}
                 <Outlet /> {/* Rendera innehållet för respektive sida */}
             </main>
             <Footer /> {/* Rendera Footer */}
