538b7951e490509da53ae0b149f304d78f94c551
Isa Westling <isawestling.blogg@hotmail.com>
2025-03-04 21:13:36 +0100

    Ändrat interface till _id istället för id, lagt till bookTitle och ändrat userId till firstname.

--- src/types/review.types.ts
+++ src/types/review.types.ts
@@ -1,8 +1,9 @@
 // Interface för recensioner
 export interface Review {
-    id: string; // Unikt ID för recensionen
+    _id: string; // Unikt ID för recensionen
     bookId: string; // ID för boken som recensionen gäller
-    userId: { _id: string; name: string }; // ID och namn för användaren som skrev recensionen
+    bookTitle: string; // Titel för boken som recensionen gäller
+    userId: { _id: string; firstname: string }; // ID och namn för användaren som skrev recensionen
     reviewText: string;
     rating: number;
     createdAt: string;
