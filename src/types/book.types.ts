573db36d17421a87634e49da887149d98977dcf3
Isa Westling <isawestling.blogg@hotmail.com>
2025-03-05 16:03:24 +0100

    Skapat ett interface för en bokgillning

--- src/types/book.types.ts
+++ src/types/book.types.ts
@@ -1,21 +1,27 @@
 // Interface för att representera böcker från Google Books API
 export interface Book {
-    id: string; // Unikt ID för boken
-    volumeInfo: {
-      title: string; // Titel på boken
-      subtitle?: string; // Underrubrik
-      authors?: string[]; // Författare
-      description?: string; // Beskrivning av boken
-      imageLinks?: {
-        thumbnail?: string; // Länk till bokens framsida
-        small?: string;
-        medium?: string;
-        large?: string;
-      };
-      publishedDate?: string; // Publiceringsdatum
-      publisher?: string; // Förlaget som gav ut boken
-      pageCount?: number; // Antal sidor
-      categories?: string[]; // Genre/kategorier för boken
-      language?: string; // Språk
+  id: string; // Unikt ID för boken
+  volumeInfo: {
+    title: string; // Titel på boken
+    subtitle?: string; // Underrubrik
+    authors?: string[]; // Författare
+    description?: string; // Beskrivning av boken
+    imageLinks?: {
+      thumbnail?: string; // Länk till bokens framsida
+      small?: string;
+      medium?: string;
+      large?: string;
     };
-  }
+    publishedDate?: string; // Publiceringsdatum
+    publisher?: string; // Förlaget som gav ut boken
+    pageCount?: number; // Antal sidor
+    categories?: string[]; // Genre/kategorier för boken
+    language?: string; // Språk
+  };
+}
+
+// Interface för en bokgillning
+export interface BookLike {
+  userId: string; // Användar-ID
+  bookId: string; // Bok-ID för google books
+}
