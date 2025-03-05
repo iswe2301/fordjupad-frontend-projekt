ec92c5120e43dc7efb29ff7b50231d096e38c3b5
Isa Westling <isawestling.blogg@hotmail.com>
2025-03-05 13:00:55 +0100

    Uppdaterat användarobjekt i Review-typen för att inkludera efternamn och e-postadress.

--- src/types/review.types.ts
+++ src/types/review.types.ts
@@ -3,7 +3,7 @@ export interface Review {
     _id: string; // Unikt ID för recensionen
     bookId: string; // ID för boken som recensionen gäller
     bookTitle: string; // Titel för boken som recensionen gäller
-    userId: { _id: string; firstname: string }; // ID och namn för användaren som skrev recensionen
+    userId: { _id: string; firstname: string; lastname: string; email: string; }; // Användarobjekt med ID, förnamn, efternamn och e-post
     reviewText: string;
     rating: number;
     createdAt: string;
