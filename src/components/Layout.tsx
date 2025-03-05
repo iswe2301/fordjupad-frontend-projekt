797907db0e7c0799be39677c8a4ca09c34285692
Isa Westling <isawestling.blogg@hotmail.com>
2025-03-05 20:23:14 +0100

    Lagt till ScrollToTop-komponent för att scrolla till toppen vid sidbyte

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import Header from "./Header" // Importera Header
 import Footer from "./Footer" // Importera Footer
 import { Outlet } from "react-router-dom"
+import ScrollToTop from "./ScrollToTop"
 
 // Layout-komponent
 const Layout = () => {
     return (
         <>
+            <ScrollToTop /> {/* Rendera ScrollToTop för att scrolla till toppen vid sidbyte */}
             <Header /> {/* Rendera Header */}
             <main className="container"> {/* Container för innehållet */}
                 <Outlet /> {/* Rendera innehållet för respektive sida */}
