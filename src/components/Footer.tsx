e79aae03c47df6dd3a4d6a1528ea9b212b0879c9
Isa Westling <isawestling.blogg@hotmail.com>
2025-03-05 19:37:47 +0100

    Lagt till ikoner, funktionalitet för att visa aktuellt årtal och adderat kontaktinformation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,19 @@
 const Footer = () => {
+
+  // Hämta aktuellt årtal
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-dark text-light py-3 mt-5">
       <div className="container text-center">
-        <p className="mb-0">© 2025 Isa Westling</p>
+        <p className="mb-4 mt-2">
+          <i className="bi bi-envelope-fill me-2"></i>
+          <a href="mailto:support@bookfinder.com" className="text-light">support@bookfinder.com</a>
+        </p>
+        {/* Visa aktuellt årtal */}
+        <p className="mb-0 small">
+          <i className="bi bi-c-circle"></i> {currentYear} BookFinder
+        </p>
       </div>
     </footer>
   );
