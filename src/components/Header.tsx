2271ce96bac1a726761c0a8b261280ce3373b394
Isa Westling <isawestling.blogg@hotmail.com>
2025-03-05 19:38:07 +0100

    Lagt till ikoner i navigeringsmenyn och justerat marginaler

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,10 +8,10 @@ const Header = () => {
 
   return (
     <header>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4 px-4">
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4 px-4">
         <div className="container-fluid d-flex justify-content-between align-items-center">
           {/* Länk till startsidan */}
-          <NavLink to="/" className="navbar-brand">BookFinder</NavLink>
+          <NavLink to="/" className="navbar-brand"><i className="bi bi-book"></i> BookFinder</NavLink>
 
           {/* Knapp för att visa hamburgermeny på mobil */}
           <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -22,13 +22,13 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto d-flex align-items-center">
               <li className="nav-item">
-                <NavLink to="/" className="nav-link mx-3">Hem</NavLink>
+                <NavLink to="/" className="nav-link mx-4"><i className="bi bi-house-door"></i> Hem</NavLink>
               </li>
 
               {/* Om användaren är inloggad, visa länk till /mina-sidor */}
               {user && (
                 <li className="nav-item">
-                  <NavLink to="/mina-sidor" className="nav-link mx-3">Mina sidor</NavLink>
+                  <NavLink to="/mina-sidor" className="nav-link mx-4"><i className="bi bi-person"></i> Mina sidor</NavLink>
                 </li>
               )}
 
@@ -36,15 +36,15 @@ const Header = () => {
               {!user ? (
                 <>
                   <li className="nav-item">
-                    <NavLink to="/logga-in" className="nav-link mx-3">Logga in</NavLink>
+                    <NavLink to="/logga-in" className="nav-link mx-4"><i className="bi bi-box-arrow-in-right"></i> Logga in</NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink to="/registrera" className="nav-link mx-3">Registrera</NavLink>
+                    <NavLink to="/registrera" className="nav-link ms-4 last"><i className="bi bi-person-plus"></i> Registrera</NavLink>
                   </li>
                 </>
               ) : (
                 <li className="nav-item">
-                  <button onClick={logout} className="btn btn-outline-light">Logga ut</button>
+                  <button onClick={logout} className="btn btn-outline-light ms-4 last"><i className="bi bi-box-arrow-right"></i> Logga ut</button>
                 </li>
               )}
             </ul>
